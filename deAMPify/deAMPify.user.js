8475939ce1293e962b8a310d95423258aad7a77d
zpangwin <anon@dont.email.me>
2020-06-02 14:24:13 -0400

    ported script from mobile; updated author name from "Me" to actual name, gave it a better description than "fuck amp", changed version to be date-based :-)

---
+++ deAMPify/deAMPify.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name			deAMPify
+// @namespace		deAMPify
+// @version			2020.06.02.1422
+// @description		I really hate AMP (Accelerated Mobile Pages) for a variety of reasons including the long,
+//					obfuscated links and the inability to use dynamic page elements. This script is designed
+//					to detect an AMP-link, then determine and load the non-AMP version of the page instead.
+//					I use it on my phone by loading links from Google News into the Kiwi browser to deAMPify
+//					them before sharing.
+// @author			[Ross Zilligen](https://github.com/ross-zilligen)
+// @include			http://*ampshare=*
+// @include			https://*ampshare=*
+// @include			https://*.ampproject.org/*
+// @run-at			document-start
+// @grant			none
+// ==/UserScript==
+/*
+
+SAMPLE 001:
+https://www-zdnet-com.cdn.ampproject.org/v/s/www.zdnet.com/
+       google-amp/pictures/linux-gaming-made-easy-the-
+       fastest-way-to-get-up-and-running/
+       ?usqp=mq331AQCKAE%
+       3D&amp_js_v=0.1#referrer=https%3A%2F%2Fwww.google.com
+       &amp_tf=From%20%251%24s
+       &ampshare=https%3A%2F%2Fwww.zdnet.com%2Fpictures%2F
+       linux-gaming-made-easy-the-fastest-way-to-get-up-and-
+       running%2F
+
+*/
+
+var ampAddr = String(window.location);
+if (! /.*ampshare.*/gi.test(ampAddr)) {
+    return;
+}
+var cleanAddr = ampAddr.replace(/^.*[?&]ampshare=([^&]+).*$/g, '$1');
+cleanAddr = decodeURI(cleanAddr);
+cleanAddr = decodeURIComponent(cleanAddr);
+window.location = cleanAddr;
